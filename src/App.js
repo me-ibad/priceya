521777b5cac51b48722ede4ffbb2eff1e32b56ac
Akash Vashishtha <88075926+flyingthunderrgod@users.noreply.github.com>
2021-12-10 13:15:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,33 +2,67 @@
 import './App.css';
 // import logo from './priceya-text-logo.png';
 import ProductPage2 from './ProductPage2';
+import { useState, useEffect } from 'react'
+import {  Link } from "react-router-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header3 from './Header3';
+import logo from './priceya-text-logo.png';
+import './Header.css'
+import './MultiLevelNav.css'
 
 function App() {
-  // const ks = (path) => {
-  //   return(<Link to={path}>
-  //       <img 
-  //           className="header__logo"
-  //         src={logo}
-  //       ></img>
-  //   </Link>)
-  // }
+  
+  const [showMenu, setShowMenu] = useState(false);
+  const [filterVariable, setFilterVariable] = useState('Full Category');
+
+  const linkComp = (path, value) => {
+    return(
+        <Link to={path}>
+            <a className="sub__menu-item2">
+                {value.replaceAll("-", " ")}
+            </a>
+        </Link>
+    )
+  }
+  const toggleMenu = () => {
+    if(showMenu == false){
+        setShowMenu(true);
+    }else{
+        setShowMenu(false);
+    }
+    console.log(showMenu)
+  }
+  function returnFilterVariable(){
+    return filterVariable;
+  }
+  
+  const changeFilterVariable = (value) => {
+    setFilterVariable(value)
+  }
+  
+  const ks = (path) => {
+    return(<Link to={path}>
+        <img 
+            className="header__logo"
+          src={logo}
+        ></img>
+    </Link>)
+  }
+
 
   return (
-
-    
       <Router>
         <div className="app">
-          {/* Home */}
-              {/* <Header3
-                linkComponent = {linkComp}
-                // changeFilterVariable = {changeFilterVariable}
-                // returnFilterValue = {returnFilterVariable}
-                logoX = {ks("/")}
-                redirectionList = {li}
-                toggle={toggleMenu}
-                /> */}
-            {/* 'Apple-iPhone-11-Mobile-Phone-': {'key1': 'phones', 'key2': 'mobile-phones'}, */}
+          
+          <Header3
+              linkComponent = {linkComp}
+              changeFilterVariable = {changeFilterVariable}
+              returnFilterValue = {returnFilterVariable}
+              logoX = {ks("/")}
+              
+              toggle={toggleMenu}
+              />
+            
             <Switch>
               <Route path='/' exact={true}> 
                 <ProductPage2 name="Apple-iPhone-11-Mobile-Phone-" key1="phones" key2 = "mobile-phones" />
