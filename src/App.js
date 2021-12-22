c2d402a80007bfa968c867290d2454dcb84df3db
Muhammad Saeed <62692034+me-saeed@users.noreply.github.com>
2021-12-23 02:57:20 +0500

    chandes

--- src/App.js
+++ src/App.js
@@ -1,78 +1,77 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 // import logo from './priceya-text-logo.png';
-import ProductPage2 from './ProductPage2';
-import { useState, useEffect } from 'react'
-import {  Link } from "react-router-dom";
+import ProductPage2 from "./ProductPage2";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header3 from './Header3';
-import logo from './priceya-text-logo.png';
-import './Header.css'
-import './MultiLevelNav.css'
+import Header3 from "./Header3";
+import logo from "./priceya-text-logo.png";
+import "./Header.css";
+import "./MultiLevelNav.css";
+import ProductCategory2 from "./ProductCategory2";
 
 function App() {
-  
   const [showMenu, setShowMenu] = useState(false);
-  const [filterVariable, setFilterVariable] = useState('Full Category');
+  const [filterVariable, setFilterVariable] = useState("Full Category");
 
   const linkComp = (path, value) => {
-    return(
-        <Link to={path}>
-            <a className="sub__menu-item2">
-                {value.replaceAll("-", " ")}
-            </a>
-        </Link>
-    )
-  }
+    return (
+      <Link to={path}>
+        <a className="sub__menu-item2">{value.replaceAll("-", " ")}</a>
+      </Link>
+    );
+  };
   const toggleMenu = () => {
-    if(showMenu == false){
-        setShowMenu(true);
-    }else{
-        setShowMenu(false);
+    if (showMenu == false) {
+      setShowMenu(true);
+    } else {
+      setShowMenu(false);
     }
-    console.log(showMenu)
-  }
-  function returnFilterVariable(){
+    console.log(showMenu);
+  };
+  function returnFilterVariable() {
     return filterVariable;
   }
-  
-  const changeFilterVariable = (value) => {
-    setFilterVariable(value)
-  }
-  
-  const ks = (path) => {
-    return(<Link to={path}>
-        <img 
-            className="header__logo"
-          src={logo}
-        ></img>
-    </Link>)
-  }
 
+  const changeFilterVariable = (value) => {
+    setFilterVariable(value);
+  };
+
+  const ks = (path) => {
+    return (
+      <Link to={path}>
+        <img className="header__logo" src={logo}></img>
+      </Link>
+    );
+  };
 
   return (
-      <Router>
-        <div className="app">
-          
-          <Header3
-              linkComponent = {linkComp}
-              changeFilterVariable = {changeFilterVariable}
-              returnFilterValue = {returnFilterVariable}
-              logoX = {ks("/")}
-              
-              toggle={toggleMenu}
-              />
-            
-            <Switch>
-              <Route path='/' exact={true}> 
-                <ProductPage2 name="Apple-iPhone-11-Mobile-Phone-" key1="phones" key2 = "mobile-phones" />
-              </Route>
-             
-            </Switch>
-          
-        </div>
-      </Router>
-      
+    <Router>
+      <div className="app">
+        <Header3
+          linkComponent={linkComp}
+          changeFilterVariable={changeFilterVariable}
+          returnFilterValue={returnFilterVariable}
+          logoX={ks("/")}
+          toggle={toggleMenu}
+        />
+
+        <Switch>
+          <Route path="/" exact={true}>
+            <ProductPage2
+              name="Apple-iPhone-11-Mobile-Phone-"
+              key1="phones"
+              key2="mobile-phones"
+            />
+          </Route>
+
+          <Route path="/categories/audio/speakers" exact={true}>
+            <ProductCategory2 fetchURL="https://www.priceya.pixelgradient.com/audio/speakers" />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
